b58d2ed1a53e471069c8d91303f7c8486c4a0031
Zhongyang Wang 王仲揚 <tonyex52@corp.ruten.com.tw>
2017-12-28 14:45:20 +0800

    [mod] navlist style

--- src/NavList.js
+++ src/NavList.js
@@ -10,9 +10,9 @@ class NavList extends Component {
   render() {
     return this._showNavList() ? (
       <div className="nav-list">
-        <NavLink className="nav-item" to="/main"><span className="nav-name">運送方式</span></NavLink>
-        <NavLink className="nav-item" to="/sub"><span className="nav-name">付款方式</span></NavLink>
-        <NavLink className="nav-item" to="/complete"><span className="nav-name">結帳完成</span></NavLink>
+        <NavLink className="nav-item" to="/main"><div className="nav-name">運送方式</div></NavLink>
+        <NavLink className="nav-item" to="/sub"><div className="nav-name">付款方式</div></NavLink>
+        <NavLink className="nav-item" to="/complete"><div className="nav-name">結帳完成</div></NavLink>
       </div>
     ) : '';
   }
